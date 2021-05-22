3dfd4d0583de058760e4b38d6006cbd8fdcfd28a
Tyler Williams <tylerwilliams@Tylers-MacBook-Pro-2.local>
2021-05-22 16:53:19 -0600

    changed port

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(process.env.PORT) || app.listen(3000);
   })
   .catch(err => {
     console.log(err);
